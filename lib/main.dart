d285f50d1ee89355a5cd6855c5268497ac132aa2
chahnez-abbes <82553553+chahnez-abbes@users.noreply.github.com>
2021-11-26 00:02:59 +0100

    extracts the arguments

--- lib/main.dart
+++ lib/main.dart
@@ -104,6 +104,31 @@ class _MyHomePageState extends State<MyHomePage> {
 // You can pass any object to the arguments parameter.
 // In this example, create a class that contains both
 // a customizable title and message.
+// A Widget that extracts the necessary arguments from
+// the ModalRoute.
+class ExtractArgumentsScreen extends StatelessWidget {
+  const ExtractArgumentsScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute
+    // settings and cast them as ScreenArguments.
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+      ),
+      body: Center(
+        child: Text(args.message),
+      ),
+    );
+  }
+}
+
+
 class ScreenArguments {
   final String title;
   final String message;
