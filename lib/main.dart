77187185f96d94aa078073b052bfd43a32de2448
chahnez-abbes <82553553+chahnez-abbes@users.noreply.github.com>
2021-11-26 00:19:39 +0100

    Navigate to the widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pass arguments to a named route',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.deepPurple,
       ),
       home: const MyHomePage(title: 'Pass arguments to a named route'),
@@ -38,14 +30,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -60,58 +45,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'hello Sir',
-            ),
-            Text(
-              'application boddy ',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+
+
+
         ),
-      ),
+      );
 
       // This trailing comma makes auto-formatting nicer for build methods.
-    );
+
   }
 }
-// You can pass any object to the arguments parameter.
-// In this example, create a class that contains both
-// a customizable title and message.
-// A Widget that extracts the necessary arguments from
-// the ModalRoute.
+
 class ExtractArgumentsScreen extends StatelessWidget {
   const ExtractArgumentsScreen({Key? key}) : super(key: key);
 
@@ -127,13 +78,89 @@ class ExtractArgumentsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(args.title),
       ),
+      body: Center( child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // A button that navigates to a named route.
+          // The named route extracts the arguments
+          // by itself.
+          ElevatedButton(
+            onPressed: () {
+              // When the user taps the button,
+              // navigate to a named route and
+              // provide the arguments as an optional
+              // parameter.
+              Navigator.pushNamed(
+                context,
+                ExtractArgumentsScreen.routeName,
+                arguments: ScreenArguments(
+                  'Extract Arguments Screen',
+                  'This message is extracted in the build method.',
+                ),
+              );
+            },
+            child: const Text('Navigate to screen that extracts arguments'),
+          ),
+          // A button that navigates to a named route.
+          // For this route, extract the arguments in
+          // the onGenerateRoute function and pass them
+          // to the screen.
+          ElevatedButton(
+            onPressed: () {
+              // When the user taps the button, navigate
+              // to a named route and provide the arguments
+              // as an optional parameter.
+              Navigator.pushNamed(
+                context,
+                PassArgumentsScreen.routeName,
+                arguments: ScreenArguments(
+                  'Accept Arguments Screen',
+                  'This message is extracted in the onGenerateRoute '
+                      'function.',
+                ),
+              );
+            },
+            child: const Text('Navigate to a named that accepts arguments'),
+          ),
+        ],
+      ),
+      ),
+      );
+
+  }
+}
+// A Widget that accepts the necessary arguments via the
+// constructor.
+class PassArgumentsScreen extends StatelessWidget {
+  static const routeName = '/passArguments';
+
+  final String title;
+  final String message;
+
+  // This Widget accepts the arguments as constructor
+  // parameters. It does not extract the arguments from
+  // the ModalRoute.
+  //
+  // The arguments are extracted by the onGenerateRoute
+  // function provided to the MaterialApp widget.
+  const PassArgumentsScreen({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
       body: Center(
-        child: Text(args.message),
+        child: Text(message),
       ),
     );
   }
 }
-
 
 class ScreenArguments {
   final String title;
